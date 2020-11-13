23d74494ccff00cfe84113d5471bef566ae486d1
admin <admin@192.168.1.6>
2020-11-13 11:32:56 +0000

    initial commit

---
+++ src/java/login.java
@@ -0,0 +1,31 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+//public class login {
+//    String user;
+//String pass;
+//
+//Scanner scan = new Scanner(new File("File.txt"));
+//
+//while(scan.hasNext){ //While the file still has more lines remaining
+//     if(scan.nextLine() == user){
+//          if(scan.nextLine == pass){
+//                   lblDisplay.setText("Credentials Accepted.");
+//          }
+//          else{
+//               lblDisplay.setText("Please try again.");
+//          }
+//     }
+//}
+//}
